ae32765abeef0b29acc98378196d878159b93aa7
Mathias Seidler <seishin@gmail.com>
2016-04-01 10:56:52 +0200

    Add ItemSelector.

---
+++ noscrapy/selectors/item.py
@@ -0,0 +1,13 @@
+from ..selector import Selector
+
+
+class ItemSelector(Selector):
+    can_have_childs = True
+    can_have_local_childs = True
+    will_return_items = True
+
+    def _get_data(self, parent_item):
+        yield from self.get_items(parent_item)
+
+    def _get_columns(self):
+        return ()
