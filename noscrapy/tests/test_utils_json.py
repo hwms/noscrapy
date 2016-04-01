db91ec128c2894836b761348b5a049701325a0c5
Mathias Seidler <seishin@gmail.com>
2016-04-01 19:17:01 +0200

    Add json support.

---
+++ noscrapy/tests/test_utils_json.py
@@ -0,0 +1,11 @@
+from noscrapy.utils import json
+
+def test_dumps():
+    class Foo(object):
+        def __getstate__(self):
+            return {1: 2}
+
+    foo = Foo()
+    assert json.dumps(foo) == '{"1": 2}'
+    assert json.dumps(Foo) == '"<class \'noscrapy.tests.test_utils_json.test_dumps.<locals>.Foo\'>"'
+    assert json.dumps(1) == '1'
---
+++ noscrapy/utils/json.py
@@ -0,0 +1,24 @@
+from functools import partial
+import json
+
+__all__ = 'dump', 'dumps', 'load', 'loads', 'JSONDecoder', 'JSONEncoder'
+
+class JSONEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        try:
+            return getattr(obj, '__getstate__')()
+        except (AttributeError, TypeError):
+            # type error for missing argument 'self' on classes
+            pass
+        try:
+            return json.JSONEncoder.default(self, obj)
+        except TypeError:
+            pass
+        return repr(obj)
+
+dump = partial(json.dump, cls=JSONEncoder)
+dumps = partial(json.dumps, cls=JSONEncoder)
+load = json.load
+loads = json.loads
+JSONDecoder = json.JSONDecoder
